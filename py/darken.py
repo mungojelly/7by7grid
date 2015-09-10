93e0074bd6cfdb45b5056499e7033f00fa989589
Mungojelly <mungojelly@gmail.com>
2015-09-10 07:12:42 -0400

    found the right way to get long hex in colour
    I found color.hex_l which is the right way to get the six digit hex in Python's colour, yay, phew.

--- py/darken.py
+++ py/darken.py
@@ -1,5 +1,4 @@
 from colour import Color
-from colour import web2hex
 
 input = []
 while (len(input) < 49):
@@ -9,7 +8,7 @@ output = []
 for color_text in input:
     c = Color(color_text)
     c.luminance = c.luminance / 2
-    output.append(web2hex(c.hex, force_long=True))
+    output.append(c.hex_l)
 
 for _ in range(7):
     for _ in range(7):
--- py/limegray.py
+++ py/limegray.py
@@ -1,8 +1,7 @@
 from colour import Color
-from colour import web2hex
 
 for row in range(7):
     for column in Color("gray").range_to(Color("lime"),7):
-        print web2hex(column.hex, force_long=True),
+        print column.hex_l,
     print
     
