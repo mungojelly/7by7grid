4d8721a2cfd646bfe9ee927d9885f493bdd4fb91
Mungojelly <mungojelly@gmail.com>
2015-09-10 04:41:14 -0400

    everything the same interface
    I was sleepy and so I accidentally added complexity to the interface, sneaky complexity. Really 7by7grid doesn't have two interfaces where there's single colors or subgrids of colors or any other arrangement, just seven by seven grids, like the name. It's a silly simplicity sure but it's nothing if not itself so yeah.

--- py/color-to-grid.py
+++
@@ -1,9 +0,0 @@
-# Given a line containing one color,
-# repeats that color 49 times
-# (to form a full 7x7 grid).
-
-color = raw_input()
-for _ in range(7):
-    for _ in range(7):
-        print color,
-    print
