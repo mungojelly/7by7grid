bcc97bc70563274abb54c600c29a137962149c99
Mungojelly <mungojelly@gmail.com>
2015-09-10 07:29:12 -0400

    made limegray slightly prettier
    I made this ugly script slightly (slightly) prettier.

--- py/limegray.py
+++ py/limegray.py
@@ -1,7 +1,8 @@
 from colour import Color
 
+start_colors = list(Color("#A0A0A0").range_to(Color("#404040"),7))
+end_colors = list(Color("#00FF00").range_to(Color("#009900"),7))
 for row in range(7):
-    for column in Color("gray").range_to(Color("lime"),7):
+    for column in start_colors[row].range_to(end_colors[row],7):
         print column.hex_l,
     print
-    
